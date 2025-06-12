a747c4d182fbbb3be6d9ef31f055157d4859d2e0
alexander-bochkov <64841830+alexander-bochkov@users.noreply.github.com>
2025-06-12 16:04:42 +0400

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,11 @@ import { defineConfig, loadEnv } from 'vite';
 import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const { VITE_BASE_URL: BASE_URL } = loadEnv(mode, process.cwd(), 'VITE_');
+  const { VITE_BASE_URL } = loadEnv(mode, process.cwd());
 
   return {
-    base: BASE_URL,
-    plugins: [react(), svgr(), tsconfigPaths()],
+    base: VITE_BASE_URL,
     css: {
       preprocessorOptions: {
         scss: {
@@ -17,6 +15,7 @@ export default defineConfig(({ mode }) => {
         },
       },
     },
+    plugins: [react(), svgr(), tsconfigPaths()],
     resolve: {
       alias: {
         styles: '/src/styles',
